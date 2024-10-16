126e227d28b13fc2ee653fd9b3efdb4f4664b987
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2024-10-16 12:31:17 -0300

    pagina de conhecimentos quase completa

--- src/pages/conhecimentos/index.jsx
+++ src/pages/conhecimentos/index.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
+import Lottie from 'lottie-react';
+import HeadAnimation from '../../assets/head.json'; // Importa o arquivo JSON da animação Lottie
 import { Link } from 'react-router-dom';
 import styles from './styles.module.css'; // Importa o CSS Modules
 import Seta from '../../components/seta';
 import Fase from '../../components/fases';
 import puc from '../../assets/PUCRS.jpeg'; // Importa a imagem
-import poo from '../../assets/poo.jpg'
-import ferias from '../../assets/ferias.jpg'
-import ages from '../../assets/ages.jpg'
+import poo from '../../assets/poo.jpg';
+import ferias from '../../assets/ferias.jpg';
+import ages from '../../assets/ages.jpg';
 
 function Conhecimentos() {
   return (
     <div className={styles.conhecimentosContainer}>
-      <div className={styles.backButtonContainer}>
-        <Link to="/" className={styles.backButton}>
-          <Seta></Seta>
+      {/* Header fixado com animação Lottie */}
+      <header className={styles.header}>
+        <Link to="/" className={styles.lottieLink}>
+          <Lottie
+            animationData={HeadAnimation}
+            loop={false} // Desabilita o loop da animação
+            className={styles.lottie}
+          />
         </Link>
-      </div>
+      </header>
       <div className={styles.bodyContainer}>
         <div className={styles.line}>
           <div className={styles.bullet1}></div>
@@ -30,7 +37,7 @@ function Conhecimentos() {
                 titulo="PUCRS"
                 data="26/02/2023"
                 descricao="Aos meus 18 anos, entrei na faculdade de Engenharia de Software na PUCRS. Lá tive meu primeiro contato com programação, na linguagem Java com a cadeira de Fundamentos da Programação, além de outras cadeiras fundamentais sobre a computação."
-                imagem={puc} // Agora usamos a variável importada
+                imagem={puc}
                 className={styles.fase1}
               />
             </div>
@@ -38,16 +45,16 @@ function Conhecimentos() {
               <Fase
                 titulo="Alura"
                 data="06/09/2023"
-                descricao="Já no segundo semestre da faculdade, estava me aprofundando na programação com a cadeira de Programação Orientada a Objetos. Querendo saber mais sobre o conteúdo, assinei o plano da Alura, onde pude aprender muito mais rápido e de forma didática a lógica de programação, e futuramente outros conteúdos além da faculdade"
-                imagem={poo} // Substitua pela URL da imagem da Alura
+                descricao="Já no segundo semestre da faculdade, estava me aprofundando na programação com a cadeira de Programação Orientada a Objetos. Querendo saber mais sobre o conteúdo, assinei o plano da Alura, onde pude aprender muito mais rápido e de forma didática a lógica de programação, e futuramente outros conteúdos além da faculdade."
+                imagem={poo}
               />
             </div>
             <div className={styles.fase3}>
               <Fase
                 titulo="Férias"
                 data="13/12/2023"
-                descricao="Durante o período das férias de verão pude aproveitar muito os cursos da Alura e aprender conteúdos fora da faculdade. Foi quando conheci o desenvolvimento web. Aprendi HTML, CSS e Javascript, e comecei a fazer meus primeiros projetos pessoais"
-                imagem={ferias} // Substitua pela URL da imagem das férias
+                descricao="Durante o período das férias de verão pude aproveitar muito os cursos da Alura e aprender conteúdos fora da faculdade. Foi quando conheci o desenvolvimento web. Aprendi HTML, CSS e Javascript, e comecei a fazer meus primeiros projetos pessoais."
+                imagem={ferias}
               />
             </div>
             <div className={styles.fase4}>
@@ -55,7 +62,7 @@ function Conhecimentos() {
                 titulo="AGES"
                 data="26/02/2024"
                 descricao="Já aos 19 anos, no terceiro semestre, comecei a cursar a cadeira ”AGES 1” (Agência Experimental de Software), que foi para mim a melhor cadeira da faculdade até hoje. Nela, junto com meu time, recebemos o cliente Vinicius da Rosa que foi nosso stakeholder para o projeto Polymatech."
-                imagem={ages} // Substitua pela URL da imagem da AGES
+                imagem={ages}
               />
             </div>
           </div>
