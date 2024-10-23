cb6c4aa0c514c1389bbd9def3c7e599c12bc8984
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2024-10-23 15:41:56 -0300

    certificados

---
+++ src/components/certificados/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import styles from './styles.module.css'; // Estilos para o componente
+
+const Certificados = ({ certificadosFotos, margem }) => {
+  return (
+    <div style={{ marginBottom: margem }} className={styles.container}>
+      <Marquee gradient={false} speed={50} direction="left">
+        {certificadosFotos.map((certificado, index) => (
+          <div key={index} className={styles.certificado}>
+            <img src={certificado} alt={`Certificado ${index + 1}`} />
+          </div>
+        ))}
+      </Marquee>
+    </div>
+  );
+};
+
+export default Certificados;
--- src/pages/conhecimentos/index.jsx
+++ src/pages/conhecimentos/index.jsx
@@ -13,9 +13,20 @@ import CertificadosRocketseat from '../../components/certificadosRocketseat';
 import CardTimeline from '../../components/cardTimeline';
 import Titulo from '../../components/titulo';
 import Bolinha from '../../components/bolinha';
+import Certificados from '../../components/certificados'
+import certi1 from '../../assets/certi1.jpg';
+import certi2 from '../../assets/certi2.jpg';
+import certi3 from '../../assets/certi3.jpg';
+import certi4 from '../../assets/certi4.jpg';
+import certi5 from '../../assets/certi5.jpg';
+
+
 
 function Conhecimentos() {
+  const certificadosAlura = [certi1, certi2, certi3];
+  const certificadosRocketseat = [certi4, certi5];
   return (
+
     <div className={styles.conhecimentosContainer}>
       <header className={styles.header}>
         <Link to="/" className={styles.link}>
@@ -36,29 +47,31 @@ function Conhecimentos() {
         <div className={styles.titulos}>
           <Titulo margem="634px" titulo="PUCRS" subtitulo="26/02/2023" />
           <Titulo margem="634px" titulo="Alura" subtitulo="06/09/2023" />
-          <Titulo margem="630px" titulo="Férias" subtitulo="13/12/2023" />
-          <Titulo margem="672px" titulo="AGES" subtitulo="26/02/2024" />
-          <Titulo margem="668px" titulo="Rocketseat" subtitulo="23/03/2024" />
-          <Titulo margem="568px" titulo="Projeto Destaque" subtitulo="04/07/2024" />
+          <Titulo margem="956px" titulo="Férias" subtitulo="13/12/2023" />
+          <Titulo margem="682px" titulo="AGES" subtitulo="26/02/2024" />
+          <Titulo margem="1010px" titulo="Rocketseat" subtitulo="23/03/2024" />
+          <Titulo margem="532px" titulo="Projeto Destaque" subtitulo="04/07/2024" />
           <Titulo margem="160px" titulo="Memorisely" subtitulo="04/07/2024" />
         </div>
         <div className={styles.line}>
           <Bolinha margem="692px" />
           <Bolinha margem="688px" />
-          <Bolinha margem="678px" />
-          <Bolinha margem="730px" />
-          <Bolinha margem="730px" />
-          <Bolinha margem="670px" />
+          <Bolinha margem="1008px" />
+          <Bolinha margem="740px" />
+          <Bolinha margem="1080px" />
+          <Bolinha margem="624px" />
 
 
         </div>
         <div className={styles.cards}>
           <CardTimeline margem="160px" imagem={puc} texto="Aos meus 18 anos, entrei na faculdade de Engenharia de Software na PUCRS. Lá tive meu primeiro contato com programação, na linguagem Java com a cadeira de Fundamentos da Programação, além de outras cadeiras fundamentais sobre a computação." />
           <CardTimeline margem="160px" imagem={poo} texto="Já no segundo semestre da faculdade, estava me aprofundando na programação com a cadeira de Programação Orientada a Objetos. Querendo saber mais sobre o conteúdo, assinei o plano da Alura, onde pude aprender muito mais rápido e de forma didática a lógica de programação, e futuramente outros conteúdos além da faculdade." />
-          <CardTimeline margem="160px" imagem={ferias} texto="Durante o período das férias de verão pude aproveitar muito os cursos da Alura e aprender conteúdos fora da faculdade. Foi quando conheci o desenvolvimento web. Aprendi HTML, CSS e Javascript, e comecei a fazer meus primeiros projetos pessoais." />
+          <CardTimeline margem="60px" imagem={ferias} texto="Durante o período das férias de verão pude aproveitar muito os cursos da Alura e aprender conteúdos fora da faculdade. Foi quando conheci o desenvolvimento web. Aprendi HTML, CSS e Javascript, e comecei a fazer meus primeiros projetos pessoais." />
+          <Certificados margem="160px" certificadosFotos={certificadosAlura} />
           <CardTimeline margem="160px" imagem={ages} texto="Já aos 19 anos, no terceiro semestre, comecei a cursar a cadeira AGES 1 (Agência Experimental de Software), que foi para mim a melhor cadeira da faculdade até hoje. Nela, recebemos um cliente e trabalhamos no método SCRUM para desenvolver um MVP da proposta trazida pelo cliente. O nosso projeto,
 Polimatech, era uma plataforma para vestibulandos e estudantes do ensino médio." />
-          <CardTimeline margem="160px" imagem={rocketseat} texto="Após decidirmos as tecnologias na AGES, senti ainda mais a necessidade de aprender React, já queria dar esse passo no desenvolvimento web e assinei a plataforma da RocketSeat, onde já havia participado de um evento gratuito e gostei muito da didática deles. Durante esse terceiro semestre pude me aprofundar muito em React para o front-end e gostei muito de poder trabalhar em equipe na AGES." />
+          <CardTimeline margem="60px" imagem={rocketseat} texto="Após decidirmos as tecnologias na AGES, senti ainda mais a necessidade de aprender React, já queria dar esse passo no desenvolvimento web e assinei a plataforma da RocketSeat, onde já havia participado de um evento gratuito e gostei muito da didática deles. Durante esse terceiro semestre pude me aprofundar muito em React para o front-end e gostei muito de poder trabalhar em equipe na AGES." />
+          <Certificados margem="160px" certificadosFotos={certificadosRocketseat} />
           <CardTimeline margem="160px" imagem={destaque} texto="No final do semestre acontece uma votação para eleger o melhor projeto da AGES e felizmente fomos premiados! " />
           <CardTimeline margem="160px" imagem={memorisely} texto="Depois do projeto da AGES me interessei muito por UI Design, queria aprender boas práticas para criar uma interface intuitiva e aprimorar minhas habilidades com o Figma. Foi aí que assinei o Memorisely e pude me aprofundar muito com a plataforma sobre UX/UI Design." />
         </div>
